518e72e5ecc346cf9b48e35cd7e27d319f2ff386
Slajmi <info.slajmi@gmail.com>
2026-05-30 23:43:18 +0200

    Update script.js

--- public/index/script.js
+++ public/index/script.js
@@ -7,34 +7,56 @@ const video2 = "lao2.mp4";
 
 let stage = 1;
 
+async function playVideo(src) {
+  video.pause();
+  video.removeAttribute("src");
+  video.load();
+
+  video.src = src;
+
+  try {
+    await video.play();
+  } catch (err) {
+    console.error("Video play failed:", err);
+  }
+}
+
 enterBtn.onclick = async () => {
   enterBtn.style.display = "none";
 
   video.style.display = "block";
-  video.src = video1;
 
-  await video.play();
+  stage = 1;
+  ui.innerHTML = "";
+
+  await playVideo(video1);
 };
 
 video.addEventListener("ended", () => {
+  console.log("ended fired, stage =", stage);
 
   if (stage === 1) {
     ui.innerHTML = `
       <div style="display:flex; gap:15px;">
-        <button class="btn" id="nextBtn"><img src="images/yes.png" alt="Yes"></button>
-        <button class="btn" onclick="alert('Other option')"><img src="images/no.png" alt="No"></button>
+        <button class="btn" id="nextBtn">
+          <img src="images/yes.png" alt="Yes">
+        </button>
+
+        <button class="btn" onclick="alert('Other option')">
+          <img src="images/no.png" alt="No">
+        </button>
       </div>
     `;
 
     document.getElementById("nextBtn").onclick = async () => {
-      ui.innerHTML = ""; // remove buttons
+      ui.innerHTML = "";
 
       stage = 2;
-      video.src = video2;
-      await video.play();
+
+      await playVideo(video2);
     };
 
-  } else {
+  } else if (stage === 2) {
     window.location.href = "../laoPage/laoPage.html";
   }
 });
